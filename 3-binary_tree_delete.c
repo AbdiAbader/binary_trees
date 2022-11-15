7cee0e72045b97b5ee4281f75840796e6fbceeff
AbdiAbader <abdiprocode@gmail.com>
2022-11-15 11:13:39 +0300

    update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,15 @@
 #include "binary_trees.h"
 /**
- * 
+ *binary_tree_delete - deletes a tree
+ *@tree: binary tree
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
+binary_tree_t *left, *right;
 if (tree == NULL)
 return;
+if (tree->left == NULL && tree->right == NULL)
+{
 free(tree);
+}
 }
