f97aa4cdd2baf97089d3ae7a2b425892debc359b
AbdiAbader <abdiprocode@gmail.com>
2022-11-15 19:04:39 +0300

    task 9 first commit

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - perform inorder traversal
+ * @tree: pointer to binary
+ * Return: void
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t i = 0;
+if (tree == NULL)
+return (0);
+if (tree->left != NULL)
+i++;
+if (tree->right != NULL)
+i++;
+return (i);
+}
