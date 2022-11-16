5ba4e4d3a557f2983760c356410f168d9f708ed6
AbdiAbader <abdiprocode@gmail.com>
2022-11-16 13:47:10 +0300

    add hegiht functions

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,16 +6,30 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int i = 0, j = 0;
 if (tree != NULL)
 {
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+return (0);
+}
+/**
+ * binary_tree_height - perform inorder traversal
+ * @tree: pointer to binary
+ * Return: void
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t i = 0, j = 0;
+if (tree == NULL)
+return (0);
 if (tree->left != NULL)
 i++;
 if (tree->right != NULL)
 j++;
-i = i + binary_tree_balance(tree->left);
-j = j + binary_tree_balance(tree->right);
-return (i - j);
-}
-return (0);
+i = 1 + binary_tree_height(tree->left);
+j = 1 + binary_tree_height(tree->right);
+if (i > j)
+return (i);
+else
+return (j);
 }
